f5504dd9e3cfd19da08a05de237482f6b79c8534
riddickkakati <riddick.kakati@iitg.ac.in>
2024-12-25 20:29:15 +0530

    Added group creation time

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -3,6 +3,11 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
+router.register("groups", views.GroupViewset)
+router.register("members", views.MemberViewset)
+router.register("comments", views.CommentViewset)
+router.register("users", views.UserViewSet)
+router.register("profile", views.UserProfileViewset)
 router.register("compute", views.MonitoringRunViewSet)
 
 urlpatterns = [
