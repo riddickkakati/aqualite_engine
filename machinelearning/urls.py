c19475a96880bb66599e5521c7f3d1a446747466
riddickkakati <riddick.kakati@iitg.ac.in>
2025-01-06 21:34:42 +0530

    Backend completed version 1

--- machinelearning/urls.py
+++ machinelearning/urls.py
@@ -3,9 +3,9 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register("ml_groups", views.MLGroupViewset)
-router.register("ml_members", views.MLMemberViewset)
-router.register("ml_comments", views.MLCommentViewset)
+router.register("groups", views.MLGroupViewset)
+router.register("members", views.MLMemberViewset)
+router.register("comments", views.MLCommentViewset)
 router.register("users", views.UserViewSet)
 router.register("profile", views.UserProfileViewset)
 router.register("ml_analysis", views.MLRunViewSet)
